9f7f0cdbffdfd709420ef840ff43a2c6f753e816
HYF <alinamarasca@gmail.com>
2021-10-20 19:12:37 +0200

    tests for handler

--- client/src/presentation/handlers/find-animal-page/get-avatar-handler.js
+++ client/src/presentation/handlers/find-animal-page/get-avatar-handler.js
@@ -1,7 +1,19 @@
+// const getAvatarHandler = array => {
+//   const avatar = array
+//     .map(item => (item.isPrincipal ? item.image : null))
+//     .filter(item => item)[0];
+//   return avatar;
+// };
+
 const getAvatarHandler = array => {
-  const avatar = array
-    .map(item => (item.isPrincipal ? item.image : null))
-    .filter(item => item)[0];
+  let avatar;
+  if (array.length == 0) {
+    avatar = null;
+  } else {
+    avatar = array
+      .map(item => (item.isPrincipal ? item.image : null))
+      .filter(item => item)[0];
+  }
   return avatar;
 };
 
--- client/src/presentation/handlers/find-animal-page/get-avatar-handler.test.js
+++ client/src/presentation/handlers/find-animal-page/get-avatar-handler.test.js
@@ -1,10 +1,25 @@
 const getAvatarHandler = require("./get-avatar-handler.js");
 
 const arr = [
-  { image: "one", isPrincipal: true },
+  { image: "one.jpeg", isPrincipal: true },
   { image: "two.png", isPrincipal: false },
 ];
 
+const arrNoTrue = [
+  { image: "one.jpeg", isPrincipal: false },
+  { image: "two.png", isPrincipal: false },
+];
+
+const arrEmpty = [];
+
 test("takes an array of objects with two items 'image' and 'isPrincipal' and returns the first value of object where isPrinciple = true", () => {
-  expect(getAvatarHandler(arr)).toBe("one");
+  expect(getAvatarHandler(arr)).toBe("one.jpeg");
+});
+
+test("if there no isPrinciple equals to true, returns undefined", () => {
+  expect(getAvatarHandler(arrNoTrue)).toBe(undefined);
+});
+
+test("if array is empty, returns null", () => {
+  expect(getAvatarHandler(arrEmpty)).toBe(null);
 });
